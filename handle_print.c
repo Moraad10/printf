a81a203b844953720d515ecafdec77606006781e
Moraad10 <140069533+Moraad10@users.noreply.github.com>
2023-10-20 02:19:28 +0300

    Update handle_print.c

--- handle_print.c
+++ handle_print.c
@@ -1,3 +1,4 @@
+
 #include "main.h"
 /**
  * handle_print - Prints an argument based on its type
@@ -47,4 +48,3 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	}
 	return (printed_chars);
 }
-
